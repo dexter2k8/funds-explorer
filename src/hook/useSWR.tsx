c632da806b67c3270446f0430065bcd6e7ccd259
Sidnei <neimestre@yahoo.com.br>
2024-07-28 02:22:34 -0300

    implemented hook swr

---
+++ src/hook/useSWR.tsx
@@ -0,0 +1,29 @@
+import api from "@/services/api";
+import { AxiosError } from "axios";
+import fetchSWR from "swr";
+import type { KeyedMutator, SWRConfiguration } from "swr";
+
+interface IResponse<T> {
+  response: T;
+  error: AxiosError;
+  isLoading: boolean;
+  mutate: KeyedMutator<T>;
+}
+
+export function useSWR<T extends object, P = object>(
+  key?: string,
+  params?: P,
+  config?: SWRConfiguration
+): IResponse<T> {
+  const urlParams = params ? new URLSearchParams() : "";
+  const fullURL = `${key}?${urlParams}`;
+
+  const fetcher = async () => {
+    const response = await api.client.get(fullURL);
+    return response.data;
+  };
+
+  const { data: response, error, isLoading, mutate } = fetchSWR(fullURL, fetcher, config);
+
+  return { response, error, isLoading, mutate };
+}
