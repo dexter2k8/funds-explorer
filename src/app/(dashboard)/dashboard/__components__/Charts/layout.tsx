5092ef676716b40630917289f8fecbf14748dfab
Sidnei <neimestre@yahoo.com.br>
2024-08-10 00:47:36 -0300

    reverted LayoutCharts styles

--- src/app/(dashboard)/dashboard/__components__/Charts/layout.tsx
+++ src/app/(dashboard)/dashboard/__components__/Charts/layout.tsx
@@ -1,24 +1,20 @@
 import styles from "./styles.module.scss";
-import type { CSSProperties, PropsWithChildren } from "react";
+import type { PropsWithChildren } from "react";
 
 interface IChartCardProps {
   title: string;
   sideControls?: React.ReactNode;
-  height?: CSSProperties["height"];
-  overflow?: CSSProperties["overflow"];
 }
 
 export default function LayoutCharts({
   title,
   children,
   sideControls,
-  height,
-  overflow,
 }: PropsWithChildren<IChartCardProps>) {
   const { charts, head } = styles;
 
   return (
-    <div className={charts} style={{ height, overflow }}>
+    <div className={charts}>
       <div className={head}>
         <h4>{title}</h4>
         {sideControls}
