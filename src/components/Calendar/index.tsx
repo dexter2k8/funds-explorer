0040174ed4c516223daaacafe533af502ba4f436
Sidnei <neimestre@yahoo.com.br>
2024-08-12 23:04:42 -0300

    updated calendar styles

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,9 +1,16 @@
-import styles from "./styles.module.scss";
+import "./styles.scss";
 import { Calendar as ReactCalendar } from "react-calendar";
 export default function Calendar() {
   return (
-    <div className={styles.calendar}>
-      <ReactCalendar locale="en-US" />
+    <div className="calendar">
+      <ReactCalendar
+        locale="en-US" // idioma do calendário
+        minDetail="month" // mostra apenas o mês
+        next2Label={null}
+        prev2Label={null}
+        formatShortWeekday={(_, d) => d.toLocaleDateString("en-US", { weekday: "narrow" })} // muda o formato do dia da semana
+        maxDate={new Date()}
+      />
     </div>
   );
 }
