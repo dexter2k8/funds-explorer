f3a190559d131738c1b21e7e5e6492e043431212
Sidnei <neimestre@yahoo.com.br>
2024-08-22 19:04:50 -0300

    implemented head

--- src/app/(dashboard)/settings/steps/ManageFunds/index.tsx
+++ src/app/(dashboard)/settings/steps/ManageFunds/index.tsx
@@ -1,21 +1,32 @@
 "use client";
 import { useState } from "react";
+import styles from "../../styles.module.scss";
 import Table from "@/components/Table";
 import { getColumns } from "./columns";
 import { API } from "@/app/paths";
 import { useSWR } from "@/hook/useSWR";
+import { CiSquarePlus } from "react-icons/ci";
 import type { IGetFunds } from "@/app/api/get_funds/types";
 import type { IActionsProps } from "@/components/TableActions/types";
 
 export function ManageFunds() {
   const [action, setAction] = useState<IActionsProps>();
+  const { head } = styles;
   const columns = getColumns({ onAction: setAction });
 
   const { response: fundList, isLoading } = useSWR<IGetFunds[]>(API.GET_SELF_FUNDS);
 
   return (
     <div>
-      <Table isLoading={isLoading} columns={columns} rows={fundList || []} pageSize={12} />
+      <div className={head}>
+        <h4>Funds</h4>
+        <CiSquarePlus
+          size="2rem"
+          onClick={() => setAction({ action: "add", id: undefined })}
+          style={{ cursor: "pointer" }}
+        />
+      </div>
+      <Table isLoading={isLoading} columns={columns} rows={fundList || []} />
     </div>
   );
 }
